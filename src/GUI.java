5d038e05646fc9285ac2bb1ddab8a0c6d303d558
UrbanProfessional <spytron2005@gmail.com>
2022-05-23 14:59:45 +0000

    Staged Changes Test

--- src/GUI.java
+++ src/GUI.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 
 public class GUI {
     public static void main(String[] args) {
+    
+    JPanel panel = new JPanel();
     JFrame window = new JFrame();
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.setResizable(true);
@@ -9,5 +11,21 @@ public class GUI {
     window.setSize(500, 500);
     window.setLocationRelativeTo(null);
     window.setVisible(true);
+    window.add(panel);
+
+    panel.setLayout(null);
+
+    JLabel label = new JLabel("Server");
+    label.setBounds(10,20,80,25);
+    panel.add(label);
+
+    JTextField inputIP = new JTextField();
+    inputIP.setBounds(100,20,165,25);
+    panel.add(inputIP);
+
+    JLabel psswdLabel = new JLabel("Server Password");
+    psswdLabel.setBounds(10,50,80,25);
+    panel.add(psswdLabel);
+
     }
 }
