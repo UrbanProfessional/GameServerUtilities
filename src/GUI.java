8de00c4046422200bcc9567a08dde3587d5004d7
UrbanProfessional <spytron2005@gmail.com>
2022-05-23 14:41:20 +0000

    GUI Creation

---
+++ src/GUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GUI {
+    public static void main(String[] args) {
+    JFrame window = new JFrame();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(true);
+    window.setTitle("Server Utilities - By Urban");
+    window.setSize(500, 500);
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
+    }
+}
