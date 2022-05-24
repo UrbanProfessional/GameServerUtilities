eba23786759f00ab4c7cdd5ed70cb1b484dd5c9a
UrbanProfessional <spytron2005@gmail.com>
2022-05-24 14:54:23 +0000

    Ping Method Creation - WIP (Ping time incorrect/inaccurate)

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.*;
 
 public class Server {
@@ -27,7 +28,24 @@ public class Server {
         ip = ipU;
     }
 
-    public int ping() {
-        return 1;
+    public static long ping(String ip) 
+        throws UnknownHostException, IOException {
+        long ping = 0;
+        boolean up = false;
+        long sTime = System.currentTimeMillis();
+        up = InetAddress.getByName(ip).isReachable(5000);
+        long eTime = System.currentTimeMillis() - sTime;
+        ping = eTime;
+
+        if (up) {
+        return ping;
+        } else {
+        return -1;
+        }
+    }
+
+    public static void main(String[] args) 
+        throws UnknownHostException, IOException {
+        System.out.println(ping("127.0.0.1"));
     }
 }
